dfef6f899c7bd313c187ed9bf5ae6653b20e1ef1
edjlu <65055601+edjlu@users.noreply.github.com>
2020-05-08 19:57:28 -0700

    Initial Commit

---
+++ nav.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const els = [document.querySelector("h1"), ...document.querySelectorAll("h2")];
+const navLinks = document.querySelectorAll("nav div");
+
+function onScroll(evt) {
+    for (let i = 0; i < els.length; i++) {
+        const el = els[i];
+        if (window.scrollY > el.offsetTop) {
+            for (const link of navLinks) {
+                link.classList.remove("active");
+            }
+            navLinks[i].classList.add("active");
+        }
+    }
+}
+
+window.addEventListener ('scroll', onScroll);
+
