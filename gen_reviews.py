15008f56a2beb464f3783e7fe12b2d61dbf39966
edjlu <65055601+edjlu@users.noreply.github.com>
2020-05-13 17:20:25 -0700

    python template update

--- gen_reviews.py
+++ gen_reviews.py
@@ -1,38 +1,11 @@
 import os
 
-template = '''<!DOCTYPE html>
-<html>
-    <head>
-        <title> Reviews - {} </title>
-        <link rel="stylesheet" href="review.css">
-    </head>
-<body>
+t = open('template.html', 'r')
+template = t.read()
+t.close()
 
-    <nav>
-        <div>
-            <a href="index.html"> Homepage </div></a>
-        <div class="active">
-            <a href="game reviews.html" id="current"> Video Game Reviews </a>
-        </div>
-        <div>TV, Movie, Music Reviews</div>
-        <div>
-            Recipes
-        </div>
-        <div>
-            Travels
-        </div>
-        
-      </nav>
-    <div id="wrapper">
-        
-    <img src="images/horizon.jpg" class="hero" alt="cover">
 
-{}
-<h2>Click the sidebar to check out more content or return home.</h2>
-</body>
-</html>'''
-
-def gen1(name):
+def gen1(name):    
     g = open('game_reviews/{}'.format(name), encoding='utf-8')
     title = g.readline()
     body = g.read()
